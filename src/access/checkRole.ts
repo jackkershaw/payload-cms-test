88e2260a269f3f11361c9e507a460c400b0bec6e
jackkershaw <hello@jackkershaw.net>
2025-04-22 21:12:23 +0100

    check role function

---
+++ src/access/checkRole.ts
@@ -0,0 +1,17 @@
+import type { User } from 'payload'
+
+export const checkRole = (allRoles: User['roles'] = [], user: User): boolean => {
+  if (user) {
+    if (
+      allRoles.some((role: string) => {
+        return user?.roles?.some((individualRole: string) => {
+          return individualRole === role
+        })
+      })
+    ) {
+      return true
+    }
+  }
+
+  return false
+}
