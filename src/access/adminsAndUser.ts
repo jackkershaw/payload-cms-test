3530db06923100c9b8f805b1cfda2d2c6c2ddb73
jackkershaw <hello@jackkershaw.net>
2025-04-24 21:41:37 +0100

    return true for user as well as admin

--- src/access/adminsAndUser.ts
+++ src/access/adminsAndUser.ts
@@ -4,10 +4,9 @@ import { checkRole } from './checkRole'
 
 const adminsAndUser: Access = ({ req: { user } }) => {
   if (user) {
-    if (checkRole(['admin'], user)) {
+    if (checkRole(['admin', 'user'], user)) {
       return true
     }
-
     return {
       id: {
         equals: user.id,
