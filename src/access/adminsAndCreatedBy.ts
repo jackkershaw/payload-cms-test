df61c77a53881b78fd792d0c4a59fd8bb7694777
jackkershaw <hello@jackkershaw.net>
2025-04-24 16:40:50 +0100

    fix if null value for data on load

--- src/access/adminsAndCreatedBy.ts
+++ src/access/adminsAndCreatedBy.ts
@@ -7,7 +7,7 @@ const adminsAndCreatedBy: Access = ({ req: { user }, data }) => {
     if (checkRole(['admin'], user)) {
       return true
     }
-    if (user.id === data.createdBy) {
+    if (user.id === data?.createdBy) {
       return true
     }
   }
